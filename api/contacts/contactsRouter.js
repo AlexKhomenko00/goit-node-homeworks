1dad63c1f80edb413cf281187dc2028158aef875
Alex Khomenko <sasvsha@gmail.com>
2020-12-07 20:37:48 +0100

    changed hm-3

--- api/contacts/contactsRouter.js
+++ api/contacts/contactsRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const ContactsController = require("./contactsController");
+const ContactSchemas = require("../../validation/contactSchemas");
 
 const contactsRouter = express.Router();
 
@@ -8,11 +9,15 @@ contactsRouter.get("/", ContactsController.listContacts);
 
 contactsRouter.get("/:contactId", ContactsController.getContactById);
 
-contactsRouter.patch("/:contactId", ContactsController.updateContact);
+contactsRouter.patch(
+  "/:contactId",
+  ContactSchemas.validateUpdateContact,
+  ContactsController.updateContact
+);
 
 contactsRouter.post(
   "/",
-  ContactsController.validateContact,
+  ContactSchemas.validateAddContact,
   ContactsController.addContact
 );
 
